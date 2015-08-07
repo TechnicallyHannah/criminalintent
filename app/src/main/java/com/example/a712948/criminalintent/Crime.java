7ceb96a5904b57783ce996c8a37a52cb6f509b85
Hpaulson <technicalhannah@gmail.com>
2015-08-07 14:04:12 -0600

    added getters and setters

---
+++ app/src/main/java/com/example/a712948/criminalintent/Crime.java
@@ -0,0 +1,34 @@
+package com.example.a712948.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * @author Hannah Paulson
+ * @since 8/7/15.
+ */
+public class Crime {
+    ​
+
+    private UUID mId;
+    private String mTitle;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+    
+    public Crime() {
+
+        //generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    ​
+}
